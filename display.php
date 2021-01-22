b2a0281ba7e2baa5727a1a985c3e5b33a93d88d5
flobo593 <git@floribo.de>
2021-01-22 21:33:22 +0100

    .

--- display.php
+++ display.php
@@ -7,20 +7,30 @@ include 'test.php';
 <!DOCTYPE html>
 <html>
 <body>
-  
+
 
 <?php
 $sin = Module4($test1)["Sine"];
 $cos = Module4($test1)["Cosine"];
+$sin8 = Module8($teschd2)["Sine"];
+$cos8 = Module8($teschd2)["Cosine"];
 
-
-echo "Sine:";
+echo "Sine: <br />";
 for ($k=0; $k < 4; $k++) {
   echo $sin[$k]."<br />";
 }
-echo "Cosine:";
+echo "Cosine: <br />";
 for ($l=0; $l < 4; $l++) {
   echo $cos[$l]."<br />";
+}
+
+echo "Sine: <br />";
+for ($m=0; $m < 8; $m++) {
+  echo $sin8[$m]."<br />";
+}
+echo "Cosine: <br />";
+for ($n=0; $n < 8; $n++) {
+  echo $cos8[$n]."<br />";
 }
 ?>
 </body>
