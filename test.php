d63a262b4e9a39d4569d1d2b1a59968534d996e5
flobo593 <git@floribo.de>
2021-02-04 20:30:32 +0100

    .

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 $test1 = array(1,2,3,4);
-$teschd2 = [
+$teschd2 = array(
   float(1),
   float(2),
   float(3),
@@ -9,5 +9,5 @@ $teschd2 = [
   float(6),
   float(7),
   float(8)
-];
+);
 ?>
